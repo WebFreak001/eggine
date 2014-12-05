d06e9d0566ed6e49eb7bdc11acaf0ca722880c57
technocf <technocodefox@gmail.com>
2014-12-05 20:13:04 +0000

    Created Vector2d class.

--- src/com/deviotion/ld/eggine/math/Vector2d.java
+++ src/com/deviotion/ld/eggine/math/Vector2d.java
@@ -9,4 +9,34 @@ package com.deviotion.ld.eggine.math;
  */
 
 public class Vector2d {
+
+    private double x;
+    private double y;
+
+    public Vector2d (double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX () {
+        return this.x;
+    }
+    public double getY () {
+        return this.y;
+    }
+
+    public void setX (double x) {
+        this.x = x;
+    }
+    public void setY (double y) {
+        this.y = y;
+    }
+
+    public double distance (Vector2d vector) {
+        double distX = vector.getX() - this.x;
+        double distY = vector.getY() - this.y;
+
+        return Math.sqrt(distX * distX + distY * distY);
+    };
+
 }
