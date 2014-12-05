a27251eb14fd60018066661f134e0a81a5a41a3d
technocf <technocodefox@gmail.com>
2014-12-05 20:54:05 +0000

    Created loop class.

--- src/com/deviotion/ld/eggine/loop/Loop.java
+++ src/com/deviotion/ld/eggine/loop/Loop.java
@@ -1,5 +1,8 @@
 package com.deviotion.ld.eggine.loop;
 
+import com.deviotion.ld.eggine.graphics.Screen;
+import com.deviotion.ld.eggine.graphics.Window;
+
 /**
  * Eggine
  * A last minute game engine for Ludum Dare.
@@ -8,5 +11,86 @@ package com.deviotion.ld.eggine.loop;
  *
  */
 
-public class Loop {
+public abstract class Loop {
+
+    private int frameRate;
+    private int updateRate;
+    private int frames;
+    private int updates;
+    private int fps;
+    private int ups;
+    private double calculatedFrameRate;
+    private double calculatedUpdateRate;
+    private double lagRender;
+    private double lagUpdate;
+    private long lastLoop;
+    private long lastFPS;
+    private Window window;
+
+    public Loop (int fps, int ups, Window window) {
+        this.frameRate = fps;
+        this.updateRate = ups;
+        this.frames = 0;
+        this.updates = 0;
+        this.fps = 0;
+        this.ups = 0;
+        this.calculatedFrameRate = 1000000000f / this.frameRate;
+        this.calculatedUpdateRate = 1000000000f / this.updateRate;
+        this.lagRender = 0;
+        this.lagUpdate = 0;
+        this.lastLoop = 0;
+        this.lastFPS = 0;
+        this.window = window;
+    }
+
+    public int getFps () {
+        return this.fps;
+    }
+    public int getUps () {
+        return this.ups;
+    }
+    public Window getWindow() {
+        return this.window;
+    }
+
+    public void start () {
+        this.lastLoop = System.nanoTime();
+
+        while (true) {
+            long now = System.nanoTime();
+            long taken = now - this.lastLoop;
+
+            this.lagRender += taken;
+            this.lagUpdate += taken;
+
+            while (this.lagUpdate < this.calculatedUpdateRate) {
+                this.update(1000000f / this.lagUpdate);
+
+                this.lagUpdate -= this.calculatedUpdateRate;
+                this.updates++;
+            }
+
+            while (this.lagRender < this.calculatedFrameRate) {
+                this.render(window.getScreen());
+
+                this.lagRender -= this.calculatedFrameRate;
+                this.frames++;
+            }
+
+            now = System.nanoTime();
+            if (now >= this.lastFPS + 1000000000f) {
+                this.fps = this.frames;
+                this.ups = this.updates;
+
+                this.frames = 0;
+                this.updates = 0;
+
+                System.out.println("FPS: " + this.fps + ", UPS:" + this.ups);
+            }
+        }
+    }
+
+    public abstract void render (Screen screen);
+    public abstract void update (double delta);
+
 }
