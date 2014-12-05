f0c8a0d7294d175e763f1525b21c91a67c0c231e
technocf <technocodefox@gmail.com>
2014-12-05 22:36:44 +0000

    Created sprite sheet class.

--- src/com/deviotion/ld/eggine/graphics/SpriteSheet.java
+++ src/com/deviotion/ld/eggine/graphics/SpriteSheet.java
@@ -1,5 +1,8 @@
 package com.deviotion.ld.eggine.graphics;
 
+import com.deviotion.ld.eggine.math.Dimension2d;
+import com.deviotion.ld.eggine.math.Vector2d;
+
 /**
  * Eggine
  * A last minute game engine for Ludum Dare.
@@ -9,4 +12,29 @@ package com.deviotion.ld.eggine.graphics;
  */
 
 public class SpriteSheet {
+
+    private Sprite sprite;
+    private Dimension2d spriteSize;
+
+    public SpriteSheet (Sprite sprite, Dimension2d spriteSize) {
+        this.sprite = sprite;
+        this.spriteSize = spriteSize;
+    }
+
+    public Sprite getSprite () {
+        return this.sprite;
+    }
+    public Dimension2d getSpriteSize () {
+        return this.spriteSize;
+    }
+
+    public Vector2d getTileVector (int tileIndex) {
+        return new Vector2d(
+                (tileIndex % this.sprite.getDimension().getWidth()) * this
+                        .spriteSize.getWidth(),
+                (tileIndex / this.sprite.getDimension().getHeight()) * this
+                        .spriteSize.getHeight()
+        );
+    }
+
 }
