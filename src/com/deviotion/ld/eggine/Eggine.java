cc2c41ff3d8f4f97fb132b7418d402bd19db6d25
technocf <technocodefox@gmail.com>
2014-12-05 21:33:20 +0000

    Added keyboard to Eggine class.

--- src/com/deviotion/ld/eggine/Eggine.java
+++ src/com/deviotion/ld/eggine/Eggine.java
@@ -1,6 +1,7 @@
 package com.deviotion.ld.eggine;
 
 import com.deviotion.ld.eggine.graphics.Window;
+import com.deviotion.ld.eggine.input.Keyboard;
 import com.deviotion.ld.eggine.loop.Loop;
 
 /**
@@ -13,8 +14,18 @@ import com.deviotion.ld.eggine.loop.Loop;
 
 public abstract class Eggine extends Loop {
 
+    private Keyboard keyboard;
+
     public Eggine (int fps, int ups, Window window) {
         super(fps, ups, window);
+
+        this.keyboard = new Keyboard();
+
+        this.getWindow().getCanvas().addKeyListener(this.keyboard);
+    }
+
+    public Keyboard getKeyboard () {
+        return this.keyboard;
     }
 
 }
