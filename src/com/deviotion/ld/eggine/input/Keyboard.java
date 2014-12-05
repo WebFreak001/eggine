b9c718bf93aafe96f63ae6d32fba06ed3f644484
technocf <technocodefox@gmail.com>
2014-12-05 21:32:33 +0000

    Created Keyboard class.

--- src/com/deviotion/ld/eggine/input/Keyboard.java
+++ src/com/deviotion/ld/eggine/input/Keyboard.java
@@ -1,5 +1,8 @@
 package com.deviotion.ld.eggine.input;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 /**
  * Eggine
  * A last minute game engine for Ludum Dare.
@@ -8,5 +11,35 @@ package com.deviotion.ld.eggine.input;
  *
  */
 
-public class Keyboard {
+public class Keyboard implements KeyListener {
+
+    private boolean[] keys;
+
+    public Keyboard () {
+        this.keys = new boolean[65000];
+    }
+
+    @Override
+    public void keyTyped(KeyEvent event) {}
+
+    @Override
+    public void keyPressed(KeyEvent event) {
+        this.keys[event.getKeyCode()] = true;
+    }
+
+    @Override
+    public void keyReleased(KeyEvent event) {
+        this.keys[event.getKeyCode()] = false;
+    }
+
+    public boolean isPressed(int... keys) {
+        for (int key : keys) {
+            if (!this.keys[key]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
 }
