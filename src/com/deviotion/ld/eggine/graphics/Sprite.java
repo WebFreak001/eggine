a12c6a28427f5fc4f03cfedc159ed69075e3890b
technocf <technocodefox@gmail.com>
2014-12-05 22:33:15 +0000

    Added sprite class.

--- src/com/deviotion/ld/eggine/graphics/Sprite.java
+++ src/com/deviotion/ld/eggine/graphics/Sprite.java
@@ -1,5 +1,12 @@
 package com.deviotion.ld.eggine.graphics;
 
+import com.deviotion.ld.eggine.math.Dimension2d;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Eggine
  * A last minute game engine for Ludum Dare.
@@ -9,4 +16,45 @@ package com.deviotion.ld.eggine.graphics;
  */
 
 public class Sprite {
+
+    private File spriteFile;
+    private BufferedImage bufferedImage;
+    private int transparentColor;
+    private Dimension2d dimension;
+
+    public Sprite (File spriteFile, int transparentColor) {
+        this.spriteFile = spriteFile;
+
+        try {
+            this.bufferedImage = ImageIO.read(this.spriteFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        this.transparentColor = transparentColor;
+
+        this.dimension = new Dimension2d(this.bufferedImage.getWidth(), this
+                .bufferedImage.getHeight());
+    }
+    public Sprite (File spriteFile) {
+        this(spriteFile, 0xFFFF00FF);
+    }
+
+    public File getSpriteFile () {
+        return this.spriteFile;
+    }
+    public BufferedImage getBufferedImage () {
+        return this.bufferedImage;
+    }
+    public int getTransparentColor () {
+        return this.transparentColor;
+    }
+    public Dimension2d getDimension () {
+        return this.dimension;
+    }
+
+    public int getPixel (int x, int y) {
+        return this.bufferedImage.getRGB(x, y);
+    }
+
 }
