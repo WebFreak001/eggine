54160cb4f3055276bde033957ebbfeeb82b8afb8
technocf <technocodefox@gmail.com>
2014-12-05 20:16:27 +0000

    Created Dimension2d class.

--- src/com/deviotion/ld/eggine/math/Dimension2d.java
+++ src/com/deviotion/ld/eggine/math/Dimension2d.java
@@ -9,4 +9,27 @@ package com.deviotion.ld.eggine.math;
  */
 
 public class Dimension2d {
+
+    private double width;
+    private double height;
+
+    public Dimension2d (double width, double height) {
+        this.width  = width;
+        this.height = height;
+    }
+
+    public double getWidth () {
+        return this.width;
+    }
+    public double getHeight () {
+        return this.height;
+    }
+
+    public void setWidth (double width) {
+        this.width = width;
+    }
+    public void setHeight (double height) {
+        this.height = height;
+    }
+
 }
