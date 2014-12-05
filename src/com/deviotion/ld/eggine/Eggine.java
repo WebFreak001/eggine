0c99859102e7a9d65d0deb47b8e3c51ca1f59a2d
technocf <technocodefox@gmail.com>
2014-12-05 22:19:26 +0000

    Added mouse class to Eggine.

--- src/com/deviotion/ld/eggine/Eggine.java
+++ src/com/deviotion/ld/eggine/Eggine.java
@@ -2,6 +2,7 @@ package com.deviotion.ld.eggine;
 
 import com.deviotion.ld.eggine.graphics.Window;
 import com.deviotion.ld.eggine.input.Keyboard;
+import com.deviotion.ld.eggine.input.Mouse;
 import com.deviotion.ld.eggine.loop.Loop;
 
 /**
@@ -15,17 +16,24 @@ import com.deviotion.ld.eggine.loop.Loop;
 public abstract class Eggine extends Loop {
 
     private Keyboard keyboard;
+    private Mouse mouse;
 
     public Eggine (int fps, int ups, Window window) {
         super(fps, ups, window);
 
         this.keyboard = new Keyboard();
+        this.mouse = new Mouse(window.getScale());
 
         this.getWindow().getCanvas().addKeyListener(this.keyboard);
+        this.getWindow().getCanvas().addMouseListener(this.mouse);
+        this.getWindow().getCanvas().addMouseMotionListener(this.mouse);
     }
 
     public Keyboard getKeyboard () {
         return this.keyboard;
+    }
+    public Mouse getMouse () {
+        return this.mouse;
     }
 
 }
