8a887c5d57b1f32efc8821b1670ed51db6af98fa
WebFreak001 <janju007@web.de>
2015-02-05 18:53:04 +0100

    Added Vector functions

--- com/deviotion/ld/eggine/math/Vector2d.java
+++ com/deviotion/ld/eggine/math/Vector2d.java
@@ -37,11 +37,57 @@ public class Vector2d {
         double distY = vector.getY() - this.y;
 
         return Math.sqrt(distX * distX + distY * distY);
-    };
+    }
 
-    public void add (Vector2d vector) {
+    public Vector2d add (Vector2d vector) {
         this.x += vector.getX();
         this.y += vector.getY();
+		return this;
+    }
+
+    public Vector2d subtract (Vector2d vector) {
+        this.x -= vector.getX();
+        this.y -= vector.getY();
+		return this;
+    }
+
+    public Vector2d multiply (double value) {
+        this.x *= value;
+        this.y *= value;
+		return this;
+    }
+
+    public double getLengthSquared () {
+        return this.x * this.x + this.y * this.y;
+    }
+
+    public double getLength () {
+        return Math.sqrt(getLengthSquared());
+    }
+
+    public void normalize () {
+        double invertedLength = 1.0 / getLength();
+		this.x *= invertedLength;
+		this.y *= invertedLength;
+    }
+
+    public Vector2d normalized () {
+        Vector2d vec = copy().normalize();
+		return vec;
+    }
+
+    public Vector2d copy () {
+		return new Vector2d(this.x, this.y);
+    }
+
+    public double dot (Vector2d vector) {
+		return this.x * vector.x + this.y * vector.y;
+    }
+	
+	public static Vector2d reflect (Vector2d in, Vector2d normal) {
+    	Vector2d nIn = in.normalized();
+    	Vector2d nNormal = normal.normalized();
+    	return nIn.copy().add(nNormal.copy().multiply(-2).multiply(nNormal.dot(nIn)));
     }
 
 }
